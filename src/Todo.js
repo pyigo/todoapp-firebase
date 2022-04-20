b30866905ad37985abf94e73e11139c91ead8ce5
paulette <paulette.yigo@gmail.com>
2022-04-20 15:27:32 -0400

    added delete function

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { database } from "./firebase";
+import Button from "@material-ui/core/Button";
 
 function Todo(props) {
     return (
         <div>
-            <li>{props.text}</li>
+            <li>{props.text}
+                <Button variant="contained" color="secondary" onClick={event => database.collection('todos').doc(props.todo.id).delete()}>delete
+                </Button>
+                {/* <button onClick={event => database.collection('todos').doc(props.todo.id).delete()}>delete</button> */}
+            </li>
         </div>
     )
 }
