3863f30f5803d7ce530211e645e95aa0001051f7
paulette <paulette.yigo@gmail.com>
2022-04-20 00:03:17 -0400

    created to do lists and and input form to add to dos, created todo component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from '@material-ui/core/Button';
+
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(["Learn a new language", "Write code every day", "Read a book", "Groceries Shopping"]);
+  const [input, setInput] = useState('');
+  console.log(input)
+
+  // create a variable for the onclick event 
+  const addTodo = (event) => {
+    console.log("learn firebase")
+    event.preventDefault() //avoid our form to submit every letter we type and stop refresh the page
+
+    // the spread operator ... helps keep what is already in our state and add the new one
+    setTodos([...todos, input])  //"here is the new to do we will add through input"
+    setInput('') // clear out our input after we submit
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, World</h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add to do
+        </Button>
+        {/* <button type="submit" onClick={addTodo}>Add to do</button> */}
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
+
     </div>
   );
 }
